d5419a88f6c91b1f5358b614d81230e48cfc7479
Alvaro Vigil <alvaro128@gmail.com>
2024-10-22 16:27:15 +0800

    wordcounter fix

--- app/wordcounter/page.tsx
+++ app/wordcounter/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 
 const WordCounter = () => {
@@ -7,7 +7,7 @@ const WordCounter = () => {
   const [wordCount, setWordCount] = useState(0);
   const [charCount, setCharCount] = useState(0);
 
-  useState(() => {
+  useEffect(() => {
     calculateMetrics();
   });
 
